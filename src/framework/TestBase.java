a20fabe7c61074450c80f7e20d05eccb73ea8925
Dmytro Babyuk <d_babyuk@yahoo.com>
2019-09-10 09:03:23 +0300

    Junit established

--- src/framework/TestBase.java
+++ src/framework/TestBase.java
@@ -1,20 +1,19 @@
 package framework;
 
+import org.junit.After;
+import org.junit.Before;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 
 public class TestBase {
 
     protected WebDriver driver;
 
-    @BeforeMethod
-    public WebDriver setup() {
+    @Before
+    public void setup() {
        driver = WebDriverFactory.getWebDriver();
-       return driver;
     }
 
-    @AfterMethod
+    @After
     public void exitBrowser() {
         driver.quit();
     }
--- src/test/HomePageTest.java
+++ src/test/HomePageTest.java
@@ -1,7 +1,7 @@
 package test;
 
 import framework.TestBase;
-import org.testng.annotations.Test;
+import org.junit.Test;
 import page.HomePage;
 
 public class HomePageTest extends TestBase {
