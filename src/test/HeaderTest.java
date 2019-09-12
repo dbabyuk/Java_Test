958e8984f90e77e421cb474d9c14a8e52c848f53
Dmytro Babyuk <d_babyuk@yahoo.com>
2019-09-12 11:23:13 +0300

    Parametrization established

--- src/test/HeaderTest.java
+++ src/test/HeaderTest.java
@@ -2,8 +2,29 @@ package test;
 
 import framework.TestBase;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 public class HeaderTest extends TestBase {
+    private String textbox;
+    private String userData;
+
+    public HeaderTest(String textbox, String userData) {
+        this.textbox = textbox;
+        this.userData = userData;
+    }
+
+    @Parameterized.Parameters
+    public static Collection fields1() {
+        return Arrays.asList(new Object[][] {
+                { "FIRST_NAME", "Andrew" },
+                { "LAST_NAME", "Stuart" }
+        });
+    }
 
     @Test
     public void testSimple() {
@@ -12,10 +33,7 @@ public class HeaderTest extends TestBase {
         signIn.enter_credentials("user@gmail.com", "user");
         header.clickIcon();
         header.selectOption("Profile");
-        userProfile.enterDataInTextbox(userProfile.fields.get("FIRST_NAME"), "Andrew");
-        userProfile.enterDataInTextbox(userProfile.fields.get("LAST_NAME"), "Stuart");
-        userProfile.enterDataInTextbox(userProfile.fields.get("EMAIL"), "a@chnu.edu.ua");
-        userProfile.enterDataInTextbox(userProfile.fields.get("COUNTRY"), "USA");
+        userProfile.enterDataInTextbox(userProfile.fields.get(textbox), userData);
         userProfile.clickUpdateProfile();
 
     }
