a9d31916c4ec0f024b5c80764e83a4ece24b69c0
dbabyuk <d_babyuk@yahoo.com>
2019-09-14 17:57:34 +0300

    PrepareDB established

---
+++ src/framework/PrepareDB.java
@@ -0,0 +1,39 @@
+package framework;
+
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+class PrepareDB {
+
+    void main() throws IOException {
+
+        String[] table_list = {"users", "contact", "address", "claim", "company", "education", "job",
+                "pdf_resume", "person", "photo", "requirement", "resume", "roles",
+                "skill", "user_role", "vacancy", "vacancy_resume",
+                "verificationtoken"};
+// Establishing connection to the DB and erasing old data
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/rabotyNET", "postgres", "postgres")) {
+            Statement statement = connection.createStatement();
+            for (String i : table_list) {
+                final String commandForDeleteData = "TRUNCATE" + " public." + i + " CASCADE";
+                statement.execute(commandForDeleteData);
+            }
+// Updating the data read from backup.sql file
+            File file = new File("C:/Users/Dima/IdeaProjects/Java_Test/src/framework/backup.sql");
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            String st;
+            while ((st = br.readLine()) != null) {
+                statement.executeUpdate(st);
+                System.out.println(st);
+            }
+        }
+        catch(SQLException e){
+                System.out.println("Connection failure.");
+                e.printStackTrace();
+            }
+    }
+}
--- src/framework/TestBase.java
+++ src/framework/TestBase.java
@@ -7,16 +7,21 @@ import page.Header;
 import page.SignIn;
 import page.UserProfile;
 
+import java.io.IOException;
+
 public class TestBase {
 
     private WebDriver driver;
     protected Header header;
     protected SignIn signIn;
     protected UserProfile userProfile;
+//    private PrepareDB prepareDB;
 
     @Before
-    public void setup() {
-       driver = WebDriverFactory.getWebDriver();
+    public void setup() throws IOException {
+       PrepareDB prepareDB = new PrepareDB();
+       prepareDB.main();
+        driver = WebDriverFactory.getWebDriver();
        DriverWrapper browser = new DriverWrapper(driver);
        header = new Header(browser);
        userProfile = new UserProfile(browser);
